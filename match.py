4b415cae6031d1c8c85283c9d7f1ac162cf30490
Ryan Foulke <rfoulke@stanford.edu>
2021-12-15 12:40:07 -0800

    trying to deploy

--- match.py
+++ match.py
@@ -19,7 +19,7 @@ credentials = service_account.Credentials.from_service_account_info(
 conn = connect(credentials=credentials)
 
 
-@st.cache(ttl=600)
+# @st.cache(ttl=600)
 def run_query(query):
     rows = conn.execute(query, headers=1)
     return rows
